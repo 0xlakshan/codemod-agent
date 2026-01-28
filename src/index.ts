25377e7c4ae13376560e10efce45179508a7b888
0xluckycoder <lakshanperera.dev@gmail.com>
2026-01-28 23:39:32 +0530

    feat: Narrow down to relevant PR types

--- src/index.ts
+++ src/index.ts
@@ -37,24 +37,108 @@ async function run(): Promise<void> {
 
   try {
     const data = await app.octokit.request("/app");
+
     app.octokit.log.debug(`Authenticated as '${data.data.name}'`);
     console.log(`authenticated as ${data.data.name}`);
 
     app.webhooks.onAny(({ id, name, payload }) => {
-      console.log(name, id, payload, " event received");
+      console.log(name, id, payload.sender?.login, " event received");
     });
 
     app.webhooks.on("pull_request.opened", async ({ octokit, payload }) => {
-      console.log(
-        `Received a pull request event for #${payload.pull_request.number}`,
-      );
-      console.log("pull request payload", payload);
-      await octokit.rest.issues.createComment({
+      console.log(`pull request - #${payload.pull_request.number}`);
+
+      const listFiles = await octokit.rest.pulls.listFiles({
         owner: payload.repository.owner.login,
         repo: payload.repository.name,
-        issue_number: payload.pull_request.number,
-        body: "Hello world :)",
+        pull_number: payload.pull_request.number,
       });
+
+      console.log("listFiles.data ---> ", listFiles.data);
+
+      for (const eachFile of listFiles.data) {
+        // Only updated code gets diff checked
+        if (!["added", "modified", "changed"].includes(eachFile.status)) {
+          continue;
+        }
+
+        if (eachFile.status === "added") {
+          const fileHeadContent = await octokit.rest.repos.getContent({
+            owner: payload.repository.owner.login,
+            repo: payload.repository.name,
+            path: eachFile.filename,
+            ref: payload.pull_request.head.sha,
+            mediaType: { format: "diff" },
+          });
+
+          // Narrow down to correct TS return type
+          if (Array.isArray(fileHeadContent.data)) {
+            throw new Error("Unsupported data type");
+          }
+
+          if (fileHeadContent.data.type !== "file") {
+            throw new Error("Unsupported fileConent type");
+          }
+
+          const decodedHeadContent = Buffer.from(
+            fileHeadContent.data.content,
+            "base64",
+          ).toString("utf-8");
+
+          console.log("Decoded new file head content:\n", decodedHeadContent);
+
+          // TODO: Just apply codemods to the head
+          continue;
+        }
+
+        const fileHeadContent = await octokit.rest.repos.getContent({
+          owner: payload.repository.owner.login,
+          repo: payload.repository.name,
+          path: eachFile.filename,
+          ref: payload.pull_request.head.sha,
+          mediaType: { format: "diff" },
+        });
+
+        const fileBaseContent = await octokit.rest.repos.getContent({
+          owner: payload.repository.owner.login,
+          repo: payload.repository.name,
+          path: eachFile.filename,
+          ref: payload.pull_request.base.sha,
+          mediaType: { format: "diff" },
+        });
+
+        console.log("whole fileContent --> ", fileHeadContent);
+
+        // Narrow down to correct TS return type
+        if (
+          Array.isArray(fileHeadContent.data) ||
+          Array.isArray(fileBaseContent.data)
+        ) {
+          throw new Error("Unsupported data type");
+        }
+
+        if (
+          fileHeadContent.data.type !== "file" ||
+          fileBaseContent.data.type !== "file"
+        ) {
+          throw new Error("Unsupported fileConent type");
+        }
+
+        // TODO: Check the diff
+        // Apply the codemods to only where code has been changed & added
+        // No need to do diff checks on new files and deleted files
+        const decodedHeadContent = Buffer.from(
+          fileHeadContent.data.content,
+          "base64",
+        ).toString("utf-8");
+        const decodedBaseContent = Buffer.from(
+          fileBaseContent.data.content,
+          "base64",
+        ).toString("utf-8");
+
+        console.log("Decoded updated file base content:\n", decodedBaseContent);
+        console.log("Decoded updated file head content:\n", decodedHeadContent);
+      }
     });
 
     app.webhooks.onError((error) => {
